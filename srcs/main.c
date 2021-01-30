a0d3e63bdc53776079adcf49c57043b2b9e15777
Calixte DE NAS DE TOURRIS <calide-n@e3r1p4.42.fr>
2021-01-30 08:31:59 +0100

    changed main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/30 07:47:24 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/01/30 08:28:54 by calide-n         ###   ########.fr       */
+/*   Updated: 2021/01/30 08:31:53 by calide-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 int	main(void)
 {
-	printf("Let's crush this minishell\n");
 	printf("Let's crush this minishell\n");
 	return (2);
 }
