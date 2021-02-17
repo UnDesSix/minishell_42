6182488f52d7dcc2994ca7acdbbde7fa49a70473
Ovoda <cdetourrispro@gmail.com>
2021-02-17 13:53:31 +0100

    saving...

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+char **ft_split(char *str, char c);
+
 int main(int argc, char **argv)
 {
-	ft_split(argv[1]);
+	char **tab;
+
+	tab = ft_split(argv[1], ' ');
+	for (int i = 0; tab[i]; i++)
+		printf("[%s]\n", tab[i]);
 	return (0);
 }
