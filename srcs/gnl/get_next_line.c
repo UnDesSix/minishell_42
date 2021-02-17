76b4241a41653bc33a161c502432153266a93c87
UnDesSix <facebook.matthieu@yahoo.fr>
2021-02-17 15:58:05 +0100

    saving gnl.. still not working

--- srcs/gnl/get_next_line.c
+++ srcs/gnl/get_next_line.c
@@ -1,7 +1,16 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define BUFF_SIZE 1024
+#define BUFF_SIZE 3
+
+typedef struct	s_gnl_saver
+{
+	char	buffer[BUFF_SIZE];
+	ssize_t	read_bytes;
+	ssize_t	size_line;
+	size_t	index; // Index tells us where we are in the buffer
+}				t_gnl_saver;
 
 size_t	ft_strlen(char *str)
 {
@@ -26,6 +35,7 @@ char	*ft_strncpy(char *dest, char *src, size_t n)
 	while (i < n)
 		dest[i++] = '\0';
 	return (dest);
+}
 
 char	*realloc_str(char *old_str, size_t add_size)
 {
@@ -44,56 +54,82 @@ char	*realloc_str(char *old_str, size_t add_size)
 	if (!new_str)
 		return (NULL);
 	new_str[ft_strlen(old_str) + add_size + 1] = '\0';
-	ft_strncpy(new_str, old_str, ft_strlen(old_str);
+	ft_strncpy(new_str, old_str, ft_strlen(old_str));
 	free(old_str);
 	return (new_str);
 }
 
-char	*line_saver(char *buffer, ssize_t read_bytes, char **line)
+/*
+//	This function allocate (or reallocate) enough space to copy the buffer
+//	int final line.
+//	The return value "status" can be :
+//		- 1  : there is a '\n'
+//		- 0  : there is not a '\n'
+//		- -1 : something wrong happened
+*/
+int		line_saver(t_gnl_saver *saver, char **line)
 {
 	size_t	i;
+	size_t	j;
+	int		status;
 
-	i = 0;
-	while (i < read_bytes && buffer[i] != '\n')
+	i = saver->index;
+	while (i < saver->read_bytes && saver->buffer[i] != '\n')
 		i++;
+	if (saver->buffer[i] == '\n')
+		status = 1;
+	else
+		status = 0;
 	if (!(*line))
 	{
 		*line = realloc_str(*line, i);
-		ft_strncpy(*line, buffer, i);
-		
+		ft_strncpy(*line, saver->buffer, i - saver->index);
+		saver->size_line = i - saver->index;
+		saver->index += i;
+		return (status);
 	}
+	*line = realloc_str(*line, i);
+	ft_strncpy(*line + saver->size_line, saver->buffer + saver->index, i - saver->index);
+	saver->index += i;
+	return (status);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static char		buffer[BUFF_SIZE];
-	static size_t	index = 0;
-	ssize_t			read_bytes;
-	int				status;
+	static t_gnl_saver	saver;
+	int					status;
 
-	read_bytes = 1;
+	saver.read_bytes = 1;
+	saver.size_line = 0;
+	saver.index = 0;
+	status = 0;
 	*line = NULL;
-	while (read_bytes)
+	while (saver.read_bytes && !status)
 	{
-		read_bytes = read(fd, buffer, BUFF_SIZE);
-		status = line_saver(buffer, read_bytes, line);
-		if (read_bytes < 0)
+		saver.read_bytes = read(fd, saver.buffer, BUFF_SIZE);
+		status = line_saver(&saver, line);
+		if (saver.read_bytes < 0)
 			return (-1);
-		if (status)
 	}
-	
+	if (!saver.read_bytes)
+		return (0);
+	return (1);
 }
 
 int		main()
 {
-	char	*line = NULL;
+	char	*line;
 	int		i = 0;
 
 	while (get_next_line(0, &line) == 1)
 	{
-		printf("%s\n", line);
-		free(line);
+		if (line)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
 	}
-	free(line);
+	if (line)
+		free(line);
 	return (0);
 }
