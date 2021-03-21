5292e36e35d6a346cc3ea2369787a2a8004e08a7
UnDesSix <facebook.matthieu@yahoo.fr>
2021-03-21 18:22:05 +0100

    Deletinf the variable FLAGS in recursive functions

--- includes/header.h
+++ includes/header.h
@@ -6,7 +6,7 @@
 /*   By: calide-n <calide-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 11:31:01 by calide-n          #+#    #+#             */
-/*   Updated: 2021/03/21 17:10:39 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/03/21 18:21:42 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,6 @@
 # define STOP_ENV_VAR_CHAR " \"'$?|/	~;*[#=%!-"
 # define STDOUT 0
 # define STDIN 1
-# define RIGHT 0
-# define LEFT 1
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -69,11 +67,11 @@ int			check_ast(t_node *ast);
 */
 void		btree_prefix_count(t_node *node, int *count);
 int			ast_run(t_node *root, t_list *begin_list);
-void		btree_prefix_pipe(t_node *node, t_saver *saver, int flag);
-int			btree_prefix_exec(t_node *node, t_saver *saver, int flag);
+void		btree_prefix_pipe(t_node *node, t_saver *saver);
+int			btree_prefix_exec(t_node *node, t_saver *saver);
 void		btree_prefix_clean(t_node *node);
 int			manage_redi(t_node *node, t_saver *saver);
-int			define_std_inout(t_node *node, t_saver *saver, int flag);
+int			define_std_inout(t_node *node, t_saver *saver);
 
 /*
 **	EXPANSION
