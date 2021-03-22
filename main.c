db3d40d6e7bc20771d8dc1dc9445d09841488f30
UnDesSix <facebook.matthieu@yahoo.fr>
2021-03-22 16:31:49 +0100

    norm v2, flag errors

--- main.c
+++ main.c
@@ -6,12 +6,11 @@
 /*   By: calide-n <calide-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 19:26:04 by calide-n          #+#    #+#             */
-/*   Updated: 2021/03/21 21:48:25 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/03/22 15:44:53 by calide-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/header.h"
-#define COUNT 5
 
 void	ft_free_words(t_word *word)
 {
@@ -20,112 +19,13 @@ void	ft_free_words(t_word *word)
 	x = 0;
 	while (word[x].content)
 	{
-		//		print_word(word[x]);
 		free(word[x].content);
 		x++;
 	}
-	//	ft_putstr("\n");
 	free(word);
 }
 
-int	ft_manage_line(char *orline, t_list *begin_list)
-{
-	t_word	*word;
-	t_node *root;
-//	t_saver *saver;				//USELESS?					=>CALIXTE
-	t_ast_var	ast_var;
-//	char	*tmp; 				//USELESS?					=> CALIXTE
-	char	*line;
-
-	line = expansion(orline, begin_list);
-	/*
-	// initialize the variable 'word' to silence WARNING	=> CALIXTE
-	*/
-	word = NULL;
-	//printf("[%s]\n", line);
-	word = ft_lexer(line);
-	if (!word)
-		return (1);
-	if (!ft_check_lexer(word))
-		return (1);
-	word = sort_word(word);
-	if (!word)
-		return (1);
-	free(line);
-	root = (t_node *)malloc(sizeof(t_node));
-	ast_var.index = 0;
-	ast_var.i = 0;
-	ast(word, ast_var, root, begin_list);
-	if (check_ast(root) == -1)
-	{
-		ft_free_words(word);
-		return (1);
-	}
-	if (ast_run(root, begin_list) == -2)
-	{
-		ft_free_words(word);
-		ft_free_ast(root);
-		return (0);
-	}
-//	print2DUtil(root, 0);
-	ft_free_words(word);
-	ft_free_ast(root);
-	return (1);
-}
-
-int	divide_lines(char *line, t_list *begin_list)
-{
-	int	i;
-	int	tmp_i;
-	int	begin;
-	char	stop;
-	char	*lol = line;
-
-	i = 0;
-	begin = 0;
-	stop = ' ';
-	while (lol[i])
-	{
-		if (lol[i] == '\\')
-			i++;
-		else if (lol[i] == '"' && stop == ' ')
-			stop = lol[i];
-		else if (lol[i] == '\'' && stop == ' ')
-			stop = lol[i];
-		else if ((lol[i] == '"' || lol[i] == ' ') && stop == lol[i])
-			stop = ' ';
-		else if (lol[i] == ';' && stop == ' ')
-		{
-			if (lol[i + 1] == ';' || i == 0)
-			{
-				printf("msh: syntax error near unexpected token `;'\n");
-				g_proc.ret = 256;
-				return (1);
-			}
-			tmp_i = i;
-			while (lol[tmp_i] == ' ')
-				tmp_i++;
-			if (lol[tmp_i] == '\0')
-				break ;
-			lol[i] = '\0';
-			if (ft_manage_line(&line[begin], begin_list) == 0)
-				return (0);
-			begin = i + 1;
-		}
-		i++;
-	}
-	i--;
-	while (lol[i] == ' ')
-		i--;
-	if (begin < i)
-		if (ft_manage_line(&line[begin], begin_list) == 0)
-			return (0);
-	//	printf("%d\n", g_proc.ret);
-	return (1);
-}
-
-
-int	get_input(t_list *begin_list)
+int		get_input(t_list *begin_list)
 {
 	char	*line;
 	int		ret;
@@ -144,7 +44,7 @@ int	get_input(t_list *begin_list)
 				break ;
 			ret = divide_lines(line, begin_list);
 			if (ret == 0)
-				break;
+				break ;
 			free(line);
 		}
 		if (g_proc.ret == 0)
@@ -156,14 +56,15 @@ int	get_input(t_list *begin_list)
 	return (0);
 }
 
-int main(int argc, char **argv, char **envp)
+int		main(int argc, char **argv, char **envp)
 {
-	(void)argc;					// Should we use argc ?		=> CALIXTE
-	(void)argv;					// Should we use argv ?		=> CALIXTE
 	t_list	*begin_list;
 
+	(void)argc;
+	(void)argv;
 	g_proc.ret = 0;
 	g_proc.index = 0;
+//	g_proc.shell_lvl = 0;
 	begin_list = tabs_to_list(envp);
 	get_input(begin_list);
 	ft_list_clear(begin_list, free_var);
