a896024e92de74fa3b4b5a36a8cf031bfa38ff36
UnDesSix <facebook.matthieu@yahoo.fr>
2021-03-22 16:13:01 +0100

    Separators took into account

--- ast/ast_exec.c
+++ ast/ast_exec.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 12:02:08 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/03/21 21:57:33 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/03/21 23:41:45 by calide-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_is_cmd(char *str)
 {
-	struct	stat stats;
+	struct stat	stats;
 
 	if (stat(str, &stats) == 0)
 		return (1);
@@ -22,6 +22,23 @@ int		ft_is_cmd(char *str)
 	ft_putstr(str);
 	ft_putstr(": command not found\n");
 	return (0);
+}
+
+void	init_pid_proc(t_node *node, t_saver *saver)
+{
+	if (ft_strcmp(node->args[0], "./minishell") == 0)
+		g_proc.shell_lvl = 1;
+	node->envp = list_to_tabs(saver->envp_list, 0);
+	if (node->pfd_input != STDOUT)
+	{
+		close(0);
+		dup(node->pfd_input);
+	}
+	if (node->pfd_output != STDIN)
+	{
+		close(1);
+		dup(node->pfd_output);
+	}
 }
 
 int		exec_arg_fork(t_node *node, t_saver *saver)
@@ -36,25 +53,18 @@ int		exec_arg_fork(t_node *node, t_saver *saver)
 	}
 	if (pid == 0)
 	{
-		node->envp = list_to_tabs(saver->envp_list, 0);
-		if (node->pfd_input != STDOUT)
-		{
-			close(0);
-			dup(node->pfd_input);
-		}
-		if (node->pfd_output != STDIN)
-		{
-			close(1);
-			dup(node->pfd_output);
-		}
+		init_pid_proc(node, saver);
 		if (ft_is_builtin(node->word[0].content))
 			exit(exec_builtins(node->word, saver->envp_list));
 		else if (ft_is_cmd(node->args[0]))
 			execve(node->args[0], node->args, node->envp);
 		exit(127);
 	}
-	g_proc.pid[g_proc.index] = pid;
-	g_proc.index++;
+	if (ft_strcmp(node->args[0], "./minishell") != 0)
+	{
+		g_proc.pid[g_proc.index] = pid;
+		g_proc.index++;
+	}
 	return (0);
 }
 
@@ -65,7 +75,7 @@ int		exec_arg(t_node *node, t_saver *saver)
 	if (ft_strcmp(node->word[0].content, "echo") != 0)
 		g_proc.ret = -1;
 	if (ft_strcmp(node->word[0].content, "cd") == 0
-			|| (ft_strcmp(node->word[0].content, "export") == 0 && 
+			|| (ft_strcmp(node->word[0].content, "export") == 0 &&
 				node->word[1].content)
 			|| ft_strcmp(node->word[0].content, "unset") == 0 ||
 			ft_strcmp(node->word[0].content, "exit") == 0)
@@ -73,7 +83,7 @@ int		exec_arg(t_node *node, t_saver *saver)
 		ret = exec_builtins(node->word, saver->envp_list);
 		if (ret == -2)
 			return (-2);
-		g_proc.ret = ret; 
+		g_proc.ret = ret;
 		return (2);
 	}
 	exec_arg_fork(node, saver);
@@ -84,21 +94,14 @@ int		exec_arg(t_node *node, t_saver *saver)
 	return (0);
 }
 
-int		exec_node(t_node *node, t_saver *saver)
-{
-	if (node->type == ARG)
-		return (exec_arg(node, saver));
-	return (0);
-}
-
-
-int	btree_prefix_exec(t_node *node, t_saver *saver)
+int		btree_prefix_exec(t_node *node, t_saver *saver)
 {
 	int	ret;
 
 	if (!node)
 		return (-1);
-	ret = exec_node(node, saver);
+	if (node->type == ARG)
+		ret = exec_arg(node, saver);
 	if (node->left)
 		ret = btree_prefix_exec(node->left, saver);
 	if (node->right)
