8026c7cf799658cd57074a0a0b43b0000d38885f
UnDesSix <facebook.matthieu@yahoo.fr>
2021-03-22 16:32:04 +0100

    norm v2, flag errors

---
+++ main/divide_lines.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   divide_lines.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calide-n <calide-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/22 15:14:03 by calide-n          #+#    #+#             */
+/*   Updated: 2021/03/22 15:42:43 by calide-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/header.h"
+
+int		manage_endof_line(char *line, int i,
+		t_list *begin_list, int begin)
+{
+	i--;
+	while (line[i] == ' ')
+		i--;
+	if (begin < i)
+		if (ft_manage_line(&line[begin], begin_list) == 0)
+			return (0);
+	return (1);
+}
+
+int		check_semicolon_error(int i, char *lol)
+{
+	if (lol[i + 1] == ';' || i == 0)
+	{
+		printf("msh: syntax error near unexpected token `;'\n");
+		g_proc.ret = 256;
+		return (1);
+	}
+	return (0);
+}
+
+int		check_end_after_semicolon(char *lol, int i)
+{
+	while (lol[i] == ' ')
+		i++;
+	if (lol[i] == '\0')
+		return (0);
+	return (1);
+}
+
+void	check_ifs(char *lol, int *i, char *stop)
+{
+	if (lol[*i] == '\\')
+		*i += 1;
+	else if (lol[*i] == '"' && *stop == ' ')
+		*stop = lol[*i];
+	else if (lol[*i] == '\'' && *stop == ' ')
+		*stop = lol[*i];
+	else if ((lol[*i] == '"' || lol[*i] == ' ') && *stop == lol[*i])
+		*stop = ' ';
+}
+
+int		divide_lines(char *line, t_list *begin_list)
+{
+	int		i;
+	int		begin;
+	char	stop;
+	char	*lol;
+
+	i = -1;
+	begin = 0;
+	stop = ' ';
+	lol = line;
+	while (lol[++i])
+	{
+		check_ifs(lol, &i, &stop);
+		if (lol[i] == ';' && stop == ' ')
+		{
+			if (check_semicolon_error(i, lol))
+				return (1);
+			if (!check_end_after_semicolon(lol, i))
+				break ;
+			lol[i] = '\0';
+			if (ft_manage_line(&line[begin], begin_list) == 0)
+				return (0);
+			begin = i + 1;
+		}
+	}
+	return (manage_endof_line(line, i, begin_list, begin));
+}
---
+++ main/manage_line.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manage_line.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calide-n <calide-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/22 15:07:19 by calide-n          #+#    #+#             */
+/*   Updated: 2021/03/22 15:42:32 by calide-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/header.h"
+
+int	return_cases_ast(t_node *root, t_word *word, t_list *begin_list)
+{
+	if (check_ast(root) == -1)
+	{
+		ft_free_words(word);
+		ft_free_ast(root);
+		return (1);
+	}
+	if (ast_run(root, begin_list) == -2)
+	{
+		ft_free_words(word);
+		ft_free_ast(root);
+		return (0);
+	}
+	ft_free_words(word);
+	ft_free_ast(root);
+	return (1);
+}
+
+int	ft_manage_line(char *orline, t_list *begin_list)
+{
+	t_word		*word;
+	t_node		*root;
+	t_ast_var	ast_var;
+	char		*line;
+
+	word = NULL;
+	line = expansion(orline, begin_list);
+	word = ft_lexer(line);
+	if (!word)
+		return (1);
+	if (!ft_check_lexer(word))
+		return (1);
+	word = sort_word(word);
+	if (!word)
+		return (1);
+	free(line);
+	root = (t_node *)malloc(sizeof(t_node));
+	ast_var.index = 0;
+	ast_var.i = 0;
+	ast(word, ast_var, root, begin_list);
+	return (return_cases_ast(root, word, begin_list));
+}
